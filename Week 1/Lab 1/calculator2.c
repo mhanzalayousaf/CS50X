012bcc95ec0694c95afe0a5e3b023dda128a5f5f
Muhammad Hanzala <mhanzalayousaf@outlook.com>
2024-02-01 18:58:17 +0500

    Adding calculator2.c and calculator2 makefile in Lab 1

---
+++ Week 1/Lab 1/calculator2.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int add(int a, int b);
+
+int main(void)
+{
+    int x = get_int("x: ");
+    int y = get_int("y: ");
+
+    double z = (double) x / (double) y;
+    printf("%.20f\n", z);
+}
