2a62a057ab11ab69627ce9822e2c6168acf1aed4
Muhammad Hanzala <mhanzalayousaf@outlook.com>
2024-02-07 00:15:42 +0500

    Adding mario.c and mario makefile in Section directory of Lab 2

---
+++ Week 2/Lab 2/Section/mario.c
@@ -0,0 +1,12 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, string argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: ./mario number");
+    }
+    int height = atoi(argv[1]);
+}
