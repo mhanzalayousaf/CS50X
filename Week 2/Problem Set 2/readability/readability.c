6be14eb245d71b167796c2cddbda5482d0611680
Muhammad Hanzala <mhanzalayousaf@outlook.com>
2024-02-11 18:46:37 +0500

    Adding Readability directory in Problem Set 2

---
+++ Week 2/Problem Set 2/readability/readability.c
@@ -0,0 +1,84 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+int count_letters(string text);
+int count_words(string text);
+int count_sentences(string text);
+
+int main(void)
+{
+    // Prompt the user for some text
+    string text = get_string("Text: ");
+
+    // Count the number of letters, words, and sentences in the text
+    int letters = count_letters(text);
+    int words = count_words(text);
+    int sentences = count_sentences(text);
+
+    // Compute the Coleman-Liau index
+    float L = (float) letters / words * 100;
+    float S = (float) sentences / words * 100;
+    float index = 0.0588 * L - 0.296 * S - 15.8;
+
+    // Round the index to the nearest integer
+    int grade = round(index);
+
+    // Print the grade level
+    if (grade >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else if (grade < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else
+    {
+        printf("Grade %i\n", grade);
+    }
+}
+
+int count_letters(string text)
+{
+    // Return the number of letters in text
+    int count = 0;
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (isalpha(text[i]))
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int count_words(string text)
+{
+    // Return the number of words in text
+    int count = 0;
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (isspace(text[i]) || i == n - 1)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int count_sentences(string text)
+{
+    // Return the number of sentences in text
+    int count = 0;
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+        {
+            count++;
+        }
+    }
+    return count;
+}
