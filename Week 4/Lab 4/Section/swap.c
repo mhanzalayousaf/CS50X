d91b1b183636142d2afe3d7979abc6acafbc005e
Muhammad Hanzala <mhanzalayousaf@outlook.com>
2024-02-16 19:32:04 +0500

    Adding swap.c and swap makefile in Section directory of Lab 4

---
+++ Week 4/Lab 4/Section/swap.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void swap(int *a, int *b);
+
+int main(void)
+{
+    int a = 10;
+    int b = 50;
+
+    printf("a is %i, b is %i\n", a, b);
+    swap(&a, &b);
+    printf("a is %i, b is %i\n", a, b);
+}
+
+void swap(int *a, int *b)
+{
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
