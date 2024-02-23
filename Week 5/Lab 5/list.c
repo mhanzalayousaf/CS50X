19ef66111a14bae193eac94858891b6eb4387d7a
Muhammad Hanzala <mhanzalayousaf@outlook.com>
2024-02-23 19:56:34 +0500

    Adding list.c and list makefile in Lab 5

---
+++ Week 5/Lab 5/list.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // List of size 3
+    int list[3];
+
+    // Initialize list with numbers
+    list[0] = 1;
+    list[1] = 2;
+    list[2] = 3;
+
+    // Print list
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%i\n", list[i]);
+    }
+}
